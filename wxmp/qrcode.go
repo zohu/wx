f796dd8cc830db07c869fafa13a9644b62cee787
oak <ch@beituyun.com>
2022-03-15 22:22:50 +0800

    feat：完善功能

--- wxmp/qrcode.go
+++ wxmp/qrcode.go
@@ -18,12 +18,12 @@ type ParamNewQrcode struct {
 	Scene  string `json:"scene"`
 }
 type ParamQrcode struct {
-	ExpireSeconds int64  `json:"expire_seconds"` // 二维码有效时间，以秒为单位。 最大不超过2592000（即30天）
-	ActionName    string `json:"action_name"`    // 二维码类型，QR_SCENE为临时的整型参数值，QR_STR_SCENE为临时的字符串参数值，QR_LIMIT_SCENE为永久的整型参数值，QR_LIMIT_STR_SCENE为永久的字符串参数值
+	ExpireSeconds int64  `json:"expire_seconds,omitempty"` // 二维码有效时间，以秒为单位。 最大不超过2592000（即30天）
+	ActionName    string `json:"action_name"`              // 二维码类型，QR_SCENE为临时的整型参数值，QR_STR_SCENE为临时的字符串参数值，QR_LIMIT_SCENE为永久的整型参数值，QR_LIMIT_STR_SCENE为永久的字符串参数值
 	ActionInfo    struct {
 		Scene struct {
-			SceneId  int    `json:"scene_id"`  // 场景值ID，临时二维码时为32位非0整型，永久二维码时最大值为100000
-			SceneStr string `json:"scene_str"` // 场景值ID（字符串形式的ID），字符串类型，长度限制为1到64
+			SceneId  int    `json:"scene_id,omitempty"`  // 场景值ID，临时二维码时为32位非0整型，永久二维码时最大值为100000
+			SceneStr string `json:"scene_str,omitempty"` // 场景值ID（字符串形式的ID），字符串类型，长度限制为1到64
 		} `json:"scene"`
 	} `json:"action_info"` // 二维码详情
 }
