78a21fe9ccfa943c9c7c31ad098569f77daa1912
oak <ch@beituyun.com>
2022-03-30 11:38:09 +0800

    fix：gtls v0.0.5->v0.0.11

--- define.go
+++ define.go
@@ -2,16 +2,20 @@ package wx
 
 import "encoding/xml"
 
+type Response struct {
+	Errcode int64  `json:"errcode,omitempty"`
+	Errmsg  string `json:"errmsg,omitempty"`
+}
 type ParamNotify struct {
 	MsgSignature string `json:"msg_signature"`
 	Timestamp    string `json:"timestamp"`
 	Nonce        string `json:"nonce"`
-	Echostr      string `json:"echostr"`
+	Echostr      string `json:"echostr,omitempty"`
+	EncryptType  string `json:"encrypt_type,omitempty"`
 }
 
-type Response struct {
-	Errcode int64  `json:"errcode,omitempty"`
-	Errmsg  string `json:"errmsg,omitempty"`
+func (n *ParamNotify) IsSafeMode() bool {
+	return n.EncryptType == "aes"
 }
 
 // CDATA  使用该类型,在序列化为 xml 文本时文本会被解析器忽略
--- test/wxmp-msg_test.go
+++ test/wxmp-msg_test.go
@@ -2,14 +2,34 @@ package test
 
 import (
 	"github.com/hhcool/gtls/log"
+	"github.com/hhcool/gtls/utils"
+	"github.com/hhcool/wx"
+	"github.com/hhcool/wx/wxcpt"
 	"github.com/hhcool/wx/wxmp"
 	"testing"
 )
 
 func TestWxmpMsg(t *testing.T) {
-	_, err := wxmp.FindApp("wx8f4971af0e9f0c45")
+	app, err := wxmp.FindApp("wx8f4971af0e9f0c45")
 	if err != nil {
 		log.Error(err.Error())
 		return
 	}
+	msg, err1 := app.DecodeMessage(
+		&wx.ParamNotify{
+			MsgSignature: "4027de4f36110a8a910610f289cc197e60893ba5",
+			Timestamp:    "1648610886",
+			Nonce:        "1285034511",
+		},
+		&wxcpt.BizMsg4Recv{
+			Tousername: "gh_1cd4920365d4",
+			Encrypt:    "MZ0rcHt9GaEFlcrszwfIAwX6Nk/PG59nBQ9b1s13+u1rTaEDY4I/zOCV2xI97oaIR7YnopspaV4Bffnh56xmVyZV8Fu0HpLIqn57J1i8aaR+8kIcNyn+q1eXJqU0YOV7e0TigPN04z0xlQE2OTglBI2GUxsk/XX4upJ0AdpRICyKy7/7pCe0Ys9aC0NGed5GyLQbHdCwfDZnTVdIJc4Zy/xA5JDNtHU1R1JyUcVjj8FdMPHAHZ7UrbgFJDkrfqwwfAnlaRKDZkavP5gCuunStEzobiiTn8TKY+hvO6dd4F3yNNX07SufsHa7odqJhVKOFDSAZY6C6YAW0Nb2wsNHgrMvaqf2SQAYjS4wlcZpk7/02OmVYOrF/jhnbJK1I8afLE3T5eNT845fsEnsJ3sEhvTYJ8C1yPMbakll4BTVvyohRG8gIw+skMnjAc0+a1U7t0nNh9M7fRVt0mjRaofZtz0mFmmciUnBgfaFWQsWWj1OTOA0anQWlVLl/BBA966IbCRBNvWl2f/ri8vsz9hpgRCNL2R+9jsmcO+ztcdBekQ=",
+			Agentid:    "",
+		},
+	)
+	if err1 != nil {
+		log.Error(err1.Error())
+		return
+	}
+	log.Info(utils.StructToString(msg))
 }
