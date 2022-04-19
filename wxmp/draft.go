2ec824b376baab513fe5e12ce3efd6b2b466bc4f
oak <ch@beituyun.com>
2022-04-19 10:35:12 +0800

    - ‍ 新增临时素材

--- wxmp/draft.go
+++ wxmp/draft.go
@@ -1,5 +1,72 @@
 package wxmp
 
+import (
+	"fmt"
+	"github.com/hhcool/wx"
+)
+
 /**
 草稿箱
 */
+
+type ParamDraftGetAll struct {
+	Page int `json:"page"`
+	Rows int `json:"rows"`
+}
+type ResDraftGetAllWx struct {
+	wx.Response
+	TotalCount int                    `json:"total_count"`
+	ItemCount  int                    `json:"item_count"`
+	Item       []ResDraftGetAllWxItem `json:"item"`
+}
+type ResDraftGetAllWxItem struct {
+	MediaId string `json:"media_id"`
+	Content struct {
+		NewsItem []struct {
+			Title              string `json:"title"`
+			Author             string `json:"author"`
+			Digest             string `json:"digest"`
+			Content            string `json:"content"`
+			ContentSourceUrl   string `json:"content_source_url"`
+			ThumbMediaId       string `json:"thumb_media_id"`
+			ShowCoverPic       int    `json:"show_cover_pic"`
+			NeedOpenComment    int    `json:"need_open_comment"`
+			OnlyFansCanComment int    `json:"only_fans_can_comment"`
+			Url                string `json:"url"`
+		} `json:"news_item"`
+	} `json:"content"`
+	UpdateTime string `json:"update_time"`
+}
+
+// DraftGetAll
+// @Description: 查询草稿
+// @receiver ctx
+// @param h
+// @return *wx.ReturnResponseDataList
+// @return error
+func (ctx *Context) DraftGetAll(h *ParamDraftGetAll) (*wx.ReturnResponseDataList, error) {
+	if !ctx.IsMpServe() && !ctx.IsMpSubscribe() {
+		return nil, fmt.Errorf("%s 非公众号", ctx.Appid())
+	}
+	wechat := wx.NewWechat()
+	var wxr ResDraftGetAllWx
+	if err := wechat.Get(wx.ApiMp + "draft/batchget").
+		SetQuery(&wx.ParamAccessToken{AccessToken: ctx.GetAccessToken()}).
+		BindJSON(&wxr).
+		Do(); err != nil {
+		return nil, fmt.Errorf("%s 查询草稿失败 %s", ctx.Appid(), err.Error())
+	}
+	if wxr.Errcode != 0 {
+		if ctx.RetryAccessToken(wxr.Errcode) {
+			return ctx.DraftGetAll(h)
+		}
+		return nil, fmt.Errorf("%s 查询草稿失败 %s", ctx.Appid(), wxr.Errmsg)
+	}
+	res := new(wx.ReturnResponseDataList)
+	res.Page = h.Page
+	res.Rows = h.Rows
+	res.Count = wxr.ItemCount
+	res.Total = wxr.TotalCount
+	res.List = wxr.Item
+	return res, nil
+}
