e2c9b175629d489e0c73d02ccfee009e24385e74
oak <ch@beituyun.com>
2023-06-25 00:27:54 +0800

    变更

--- wx.go
+++ wx.go
@@ -11,15 +11,16 @@ import (
 )
 
 const (
-	RdsAppPrefix      = "WX_APP:"
-	RdsAppListPrefix  = "WX_APP_LIST"
-	RdsAppRetryPrefix = "WX_APP_RETRY:"
+	RdsAppPrefix      = "wx:01:"
+	RdsAppListPrefix  = "wx:02:"
+	RdsAppRetryPrefix = "wx:03:"
 )
 
 type Option struct {
 	Host     []string
 	Password string
 	Mode     string
+	Prefix   string
 }
 type Wechat struct {
 	debug    bool
@@ -46,34 +47,64 @@ func Init(op *Option) {
 		DB:       0,
 	})
 	wechat.RSet = func(k string, v interface{}, t time.Duration) *redis.StatusCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
 		return rds.Set(context.TODO(), k, v, t)
 	}
 	wechat.RGet = func(k string) *redis.StringCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
 		return rds.Get(context.TODO(), k)
 	}
 	wechat.RDel = func(k string) *redis.IntCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
 		return rds.Del(context.TODO(), k)
 	}
 	wechat.SetNX = func(k string, v interface{}, t time.Duration) *redis.BoolCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
 		return rds.SetNX(context.TODO(), k, v, t)
 	}
-	wechat.SAdd = func(key string, member ...interface{}) *redis.IntCmd {
-		return rds.SAdd(context.TODO(), key, member...)
+	wechat.SAdd = func(k string, member ...interface{}) *redis.IntCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
+		return rds.SAdd(context.TODO(), k, member...)
 	}
-	wechat.SRem = func(key string, member ...interface{}) *redis.IntCmd {
-		return rds.SRem(context.TODO(), key, member...)
+	wechat.SRem = func(k string, member ...interface{}) *redis.IntCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
+		return rds.SRem(context.TODO(), k, member...)
 	}
-	wechat.SMembers = func(key string) *redis.StringSliceCmd {
-		return rds.SMembers(context.TODO(), key)
+	wechat.SMembers = func(k string) *redis.StringSliceCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
+		return rds.SMembers(context.TODO(), k)
 	}
-	wechat.HSet = func(key string, value ...interface{}) *redis.IntCmd {
-		return rds.HSet(context.TODO(), key, value...)
+	wechat.HSet = func(k string, value ...interface{}) *redis.IntCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
+		return rds.HSet(context.TODO(), k, value...)
 	}
-	wechat.HGetAll = func(key string) *redis.StringStringMapCmd {
-		return rds.HGetAll(context.TODO(), key)
+	wechat.HGetAll = func(k string) *redis.StringStringMapCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
+		return rds.HGetAll(context.TODO(), k)
 	}
-	wechat.HIncrBy = func(key string, field string, integer int64) *redis.IntCmd {
-		return rds.HIncrBy(context.TODO(), key, field, integer)
+	wechat.HIncrBy = func(k string, field string, integer int64) *redis.IntCmd {
+		if op.Prefix != "" {
+			k = fmt.Sprintf("%s:%s", op.Prefix, k)
+		}
+		return rds.HIncrBy(context.TODO(), k, field, integer)
 	}
 	wechat.debug = op.Mode == "debug"
 	go wechat.refreshAccessToken()
